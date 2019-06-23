f4d6a740fec433f5c2dc30c754085242e26c4268
rbestardpino <49445152+rbestardpino@users.noreply.github.com>
2019-06-23 15:02:44 -0300

    PUNTO 1 hecho

--- TransportesYAlgoMas.pl
+++ TransportesYAlgoMas.pl
@@ -1,6 +1,57 @@
-% ciudad(nombre,habitantes).
-% ruta(distancia,ciudad1,ciudad2).
-% transaccion(ciudadVendedora,ciudadCompradora,cantidadProducto,transporte).
-% transporte(avion).
-% transporte(tren(CantVagones,CapacidadVagon)).
-% 
+% ciudad(Ciudad,Habitantes).
+ciudad(buenosAires,1000000). 
+ciudad(cordoba,800000).
+ciudad(laPlata,750000).
+ciudad(rosario,500000).
+ciudad(sanSalvador,450000).
+ciudad(iguazu,300000).
+ciudad(alumine,5000).
+
+% ruta(Ciudad1,Ciudad2,Distancia)
+ruta(buenosAires,cordoba,600).
+ruta(cordoba,buenosAires,600).
+ruta(buenosAires,laPlata,300).
+ruta(buenosAires,rosario,700).
+ruta(rosario,buenosAires,700).
+ruta(iguazu,buenosAires,1000).
+ruta(cordoba,rosario,200).
+ruta(sanSalvador,cordoba,600).
+ruta(laPlata,rosario,700).
+ruta(iguazu,sanSalvador,400).
+
+% transaccion(CiudadCompradora,CiudadVendedora,Producto,CantProducto,Transporte)
+transaccion(buenosAires,cordoba,harina,5000,avion).
+transaccion(cordoba,buenosAires,yerba,5000,avion).
+%transaccion(laPlata,rosario,venta(madera,4000),tren(5,900)).
+transaccion(iguazu,sanSalvador,yerba,9000,camion(mercedes)).
+transaccion(sanSalvador,cordoba,madera,8000,avion).
+transaccion(cordoba,rosario,auto,100,tren(4,700)).
+transaccion(rosario,buenosAires,auto,100,tren(4,700)).
+transaccion(iguazu,buenosAires,yerba,10000,camion(bmw)).
+
+% costoProduccion(Producto,Costo)
+costoProduccion(harina,4).
+costoProduccion(yerba,1).
+costoProduccion(auto,100000).
+costoProduccion(madera,5).
+
+% EJERCICIO 1
+
+% EL PRECIO QUE PAGA ES UNITARIO POR PRODUCTO, NO SE TIENE EN CUENTA LA CANTIDAD A COMPRAR
+
+cuantoPaga(CiudadCompradora,CiudadVendedora,Producto,CostoTotal) :-
+    costoProduccion(Producto,CostoProduccion),
+    transaccion(CiudadCompradora,CiudadVendedora,Producto,_,Transporte),
+    ruta(CiudadCompradora,CiudadVendedora,Distancia),
+    costoTransporte(CostoProduccion,Transporte,Distancia,CostoTransporte),
+    CostoTotal is (CostoProduccion + CostoTransporte).
+
+costoTransporte(CostoProduccion,tren(CantVagones,_),Distancia,CostoTransporte) :-
+    CostoTransporte is (CostoProduccion * (CantVagones + 1) * Distancia).
+costoTransporte(_,camion(mercedes),Distancia,CostoTransporte) :-
+    CostoTransporte is Distancia.
+costoTransporte(CostoProduccion,camion(Marca),_,CostoTransporte) :-
+    Marca \= mercedes,
+    CostoTransporte is (CostoProduccion * 10).
+costoTransporte(CostoProduccion,avion,_,CostoTransporte) :-
+    CostoTransporte is (CostoProduccion * 1000).
