b870b207f024d843bf59d3875c52d7928f6dcd6e
rbestardpino <49445152+rbestardpino@users.noreply.github.com>
2019-06-23 14:31:44 -0300

    arranque el parcial de mierda este

---
+++ TransportesYAlgoMas.pl
@@ -0,0 +1,6 @@
+% ciudad(nombre,habitantes).
+% ruta(distancia,ciudad1,ciudad2).
+% transaccion(ciudadVendedora,ciudadCompradora,cantidadProducto,transporte).
+% transporte(avion).
+% transporte(tren(CantVagones,CapacidadVagon)).
+% 
