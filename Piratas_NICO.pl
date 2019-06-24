1fa6ee823ad953a81e1d2ee18eaf6008a5a0bee7
Nicolas Naccachian <nanaccachian@gmail.com>
2019-06-24 10:54:36 -0300

    datos agregados

---
+++ Piratas_NICO.pl
@@ -0,0 +1,24 @@
+% DATOS
+
+%puerto(Nombre,Pais).
+
+puerto(puerto1,buenosAires).
+puerto(puerto2,bahiaBlanca).
+puerto(puerto3,florida).
+puerto(puerto4,rioDeJaneiro).
+puerto(puerto5,nicaragua).
+
+
+viaje(puerto1,puerto2,10000,galeon(10)).
+viaje(puerto2,puerto3,80000,carabela(700,60)).
+viaje(puerto4,puerto5,9000,galera(reinoUnido)).
+
+pirata(jackSparrow,barco1,350).
+pirata(nicolas,barco2,800).
+pirata(rodrigo,barco3,900).
+pirata(julian,barco4,400).
+
+impetuCombativo(barco1,100).
+impetuCombativo(barco2,300).
+impetuCombativo(barco3,200).
+impetuCombativo(barco4,500).
