ba094d4b22b6d0d69578ee474977327648f2d692
rbestardpino <49445152+rbestardpino@users.noreply.github.com>
2019-07-08 16:30:18 -0300

    parcial de fobias terminado

--- Fobias_PACO.pl
+++ Fobias_PACO.pl
@@ -1,3 +1,5 @@
+%%% PRIMERA PARTE
+
 % fobia(persona,fobia).
 fobia(maria,tecnofobia).
 fobia(maria,cyberfobia).
@@ -31,3 +33,63 @@ moderno(Persona) :-
     fobiasModernas(FobiasModernas),
     member(Fobia,FobiasModernas).
 
+% PUNTO 2
+
+contradictorio(Persona) :-
+    fobia(Persona,_),
+    findall(Fobia,fobia(Persona,Fobia),Fobias),
+    member(fobofobia,Fobias),
+    length(Fobias,Cantidad),
+    Cantidad > 1.
+    
+% PUNTO 3
+
+irracional(Persona,Fobia) :-
+    fobia(Persona,Fobia),
+    forall(experienciaTraumatica(Persona,Exp),
+            not(desencadenante(Exp,Fobia))).
+
+%%% SEGUNDA PARTE
+
+% psicologo(nombre,chamuyero(universidad,promocion)).
+% psicologo(nombre,especialista(fobiaPuntual)).
+% psicologo(nombre,experimentado([experienciasTraumaticasAtendidas])).
+psicologo(juanCarlos,experimentado([roboDeCelular, volverseLoco])).
+psicologo(marta,especialista(eufobia)).
+psicologo(gustavo,especialista(fobofobia)).
+%psicologo(jose,chamuyero(universidadFobicaArgentina,1999)).
+psicologo(ignacio,especialista(numerofobia)).
+
+% PUNTO 1
+
+puedeAtender(Psicologo,Persona,Fobia) :-
+    fobia(Persona,Fobia),
+    psicologo(Psicologo,Orientacion),
+    puedeTratar(Orientacion,Fobia).
+
+puedeTratar(chamuyero(_,_),_).
+puedeTratar(experimentado(ExperienciasTraumaticasAtendidas),Fobia) :-
+    experienciaTraumatica(_,ExperienciaTraumatica),
+    desencadenante(ExperienciaTraumatica,Fobia),
+    member(ExperienciaTraumatica,ExperienciasTraumaticasAtendidas).
+puedeTratar(especialista(Fobia),Fobia).
+
+% PUNTO 2
+
+porcentajeDeDesempleo(Porcentaje) :-
+    findall(Nombre,psicologo(Nombre,_),Psicologos),
+    length(Psicologos,Cant1),
+    findall(Nombre,(psicologo(Nombre,_),not(puedeAtender(Nombre,_,_))),PsicologosSinLaburo),
+    length(PsicologosSinLaburo,Cant2),
+    Porcentaje is ((Cant2 / Cant1) * 100).
+
+% PUNTO 3
+
+personaSana(Persona) :-
+    fobia(Persona,_),
+    forall(fobia(Persona,Fobia),
+            puedeAtender(_,Persona,Fobia)).
+
+%%% TERCERA PARTE
+
+% Tu vieja va a hacer esa justificacion
